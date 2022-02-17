32b05d5e1059ca68ea8e381f47dd2cc488a3f87b
VimalSpectus <vimal.tripathi@spectusinfotech.com>
2022-02-17 11:46:06 +0530

    h
    bh

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -7,11 +7,11 @@ function FilterButton(props) {
 
 
       
-      className="btn toggle-btn"
+      className="  btn toggle-btn"
       aria-pressed={props.isPressed}
       onClick={() => props.setFilter(props.name)}
     >
-      <span className="visually-hidden">Show </span>
+      <p className="visually-hidden">Show </p>
       <span>{props.name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>

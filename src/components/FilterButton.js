4e413f89cdb3424822995e4d2702dd519c131ca4
VimalSpectus <vimal.tripathi@spectusinfotech.com>
2022-02-17 11:05:08 +0530

    THIRD

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -3,7 +3,10 @@ import React from "react";
 function FilterButton(props) {
   return (
     <button
-      type="button"
+      type="button     "
+
+
+      
       className="btn toggle-btn"
       aria-pressed={props.isPressed}
       onClick={() => props.setFilter(props.name)}
